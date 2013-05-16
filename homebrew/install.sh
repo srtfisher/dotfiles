113ac390cae2461fefbb6300d68f738be4f4de6a
Sean Fisher <srtfisher@gmail.com>
2013-05-16 08:50:16 -0400

    Update Homebrew packages -- forgot PHP?

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -14,6 +14,7 @@ brew tap josegonzalez/homebrew-php
 # Install the packages we need
 brew install wget gist git-flow mongodb redis mysql nginx curl
 brew install imagemagick composer wp-cli node coreutils git
+brew install php54 --with-fpm php54-apc php54-mcrypt
 
 # Now Update!
 brew update #gets the information
