307b09ab01df8be27dc6379ca8c196c3287432b3
Sean Fisher <srtfisher@gmail.com>
2013-05-31 11:51:30 -0400

    Run dot every hour

--- var/launchctl.sh
+++ var/launchctl.sh
@@ -4,9 +4,15 @@
 
 VAR_BASE=$(dirname $0)
 
-
 launchctl unload ~/Library/LaunchAgents/dotfiles.srtfisher.core.plist >/dev/null
 rm -rf ~/Library/LaunchAgents/dotfiles.srtfisher.core.plist || true
 cp $VAR_BASE/dotfiles.srtfisher.core.plist ~/Library/LaunchAgents/dotfiles.srtfisher.core.plist
 
 launchctl load ~/Library/LaunchAgents/dotfiles.srtfisher.core.plist
+
+# DOT command
+launchctl unload ~/Library/LaunchAgents/dotfiles.srtfisher.dot.plist >/dev/null
+rm -rf ~/Library/LaunchAgents/dotfiles.srtfisher.dot.plist || true
+cp $VAR_BASE/dotfiles.srtfisher.dot.plist ~/Library/LaunchAgents/dotfiles.srtfisher.dot.plist
+
+launchctl load ~/Library/LaunchAgents/dotfiles.srtfisher.dot.plist
