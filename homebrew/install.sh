7d09bf80b9f459471de59079e8a56c1ac52ba748
Sean Fisher <srtfisher@gmail.com>
2013-07-24 10:27:06 -0400

    Better homebrew coverage

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -12,14 +12,15 @@ brew tap homebrew/dupes
 brew tap josegonzalez/homebrew-php
 
 # Install the packages we need
-brew install wget gist git-flow mongodb redis mysql nginx curl
-brew install imagemagick composer wp-cli node coreutils git
-brew install php54 --with-fpm php54-apc php54-mcrypt
+brew install wget gist git-flow mongodb redis mysql nginx curl hub
+brew install imagemagick composer node coreutils git
+brew install php54 --with-fpm php54-apc php54-mcrypt drush 
 
 # Now Update!
 brew update #gets the information
 brew upgrade #performs the upgrade
 
+# Install WP-CLI
 curl http://wp-cli.org/installer.sh | bash
 
 echo "Done with homebrew"
