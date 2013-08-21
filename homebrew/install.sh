a7bea17b7b7967c948cd073397b0de95620c6811
Sean Fisher <srtfisher@gmail.com>
2013-08-21 10:27:19 -0400

    Fixing /Users/seanfisher/Dropbox/sync/bin:/Users/seanfisher/.wp-cli/bin:/usr/local/share/npm/bin:/Applications/Sublime Text 2.app/Contents/SharedSupport/bin:/usr/local/sbin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -17,14 +17,12 @@ brew uninstall php54 php54-mcrypt php54-apc
 # Install the packages we need
 brew install wget gist git-flow mongodb redis mysql nginx curl hub
 brew install imagemagick composer node coreutils git
-brew install php55 --with-fpm php55-apcu php55-mcrypt php55-redis php55-xdebug php55-pcntl drush boris
+brew install php55 --with-fpm php55-apcu php55-mcrypt php55-redis php55-xdebug
 brew install php55-jsmin php55-uploadprogress php55-gearman php55-solr php55-mongo
+brew install wp-cli drush boris
 
 # Now Update!
 brew update #gets the information
 brew upgrade #performs the upgrade
-
-# Install WP-CLI
-curl http://wp-cli.org/installer.sh | bash
 
 echo "Done with homebrew"
