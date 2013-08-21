04df10d28e0f376f9d2cf4a59f2d1c316255bfce
Sean Fisher <srtfisher@gmail.com>
2013-08-21 10:05:12 -0400

    Update to PHP 5.5

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -11,10 +11,14 @@ ruby -e "$(curl -fsSL https://raw.github.com/mxcl/homebrew/go)"
 brew tap homebrew/dupes
 brew tap josegonzalez/homebrew-php
 
+# Remove old packages
+brew uninstall php54 php54-mcrypt php54-apc
+
 # Install the packages we need
 brew install wget gist git-flow mongodb redis mysql nginx curl hub
 brew install imagemagick composer node coreutils git
-brew install php54 --with-fpm php54-apc php54-mcrypt drush boris
+brew install php55 --with-fpm php55-apcu php55-mcrypt php55-redis php55-xdebug php55-pcntl drush boris
+brew install php55-jsmin php55-uploadprogress php55-gearman php55-solr php55-mongo
 
 # Now Update!
 brew update #gets the information
