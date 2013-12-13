fc3912b202c27863e6589062f0a75ec93c7e9293
Sean Fisher <srtfisher@gmail.com>
2013-12-13 12:27:15 -0500

    BIN WORK

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -15,7 +15,7 @@ brew tap josegonzalez/homebrew-php
 brew uninstall php54 php54-mcrypt php54-apc
 
 # Install the packages we need
-brew install wget gist git-flow mongodb redis mysql nginx curl hub
+brew install wget gist git-flow mongodb redis mariadb nginx curl hub
 brew install imagemagick composer node coreutils git
 brew install php55 --with-fpm php55-apcu php55-mcrypt php55-redis php55-xdebug
 brew install php55-jsmin php55-uploadprogress php55-gearman php55-solr php55-mongo
